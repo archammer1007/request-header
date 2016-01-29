5a048b0528386376ba4f237560ba6bc12542a8f6
bmartini <benmartini@shaw.ca>
2016-01-29 01:20:23 +0000

    complete

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,30 @@
-'use strict';
-
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
+module.exports = function(app){
+	//redirect requests to the main page to /whoami instead
+	app.get('/', function(request,response){
+		response.redirect('/whoami');
+	});
+	
+	app.get('/whoami', function(request,response){
+		//initialize object for return
+		var infoObj = {
+			ipaddress: null,
+			language: null,
+			software: null
 		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+		
+		//get ipaddress of request
+		infoObj.ipaddress = request.headers['x-forwarded-for'];
+		//get language of request (only the first if there are multiple)
+		infoObj.language = request.headers['accept-language'].split(',')[0];
+		
+		//get operating system info of request, using a regular expression to extract the information we want
+		var ua = request.headers['user-agent'];
+		var re = /\((.*)\)/
+		var reArray = re.exec(ua);
+		infoObj.software = reArray[1];
+		
+		//send response
+		response.send(infoObj);
+	});
+	
 };
